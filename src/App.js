7d2dab5c004d6b2ed6572eb0bb662d7dadefc978
Tejas <shimpit45@gmail.com>
2023-06-18 21:34:40 +0530

    counter app ready

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import { useState, useEffect } from 'react';
+
+
 
 const App = () => {
+
+  const [counter, setCounter] = useState(0);
+  useEffect(()=>{
+  },[counter]);
   return (
     <div className="App">
-     <h1>Hello, React!!!</h1>
+      <button onClick={() => setCounter((prevCount)=>prevCount-1 )}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount)=>prevCount+1 )}>+</button>
     </div>
   );
 }
